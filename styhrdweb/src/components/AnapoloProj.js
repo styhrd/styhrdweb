c047b298e7aac1bfd5f715cbd009c09f3feaaafb
styhrd <juanmiguelcaparas76@gmail.com>
2024-09-04 22:33:28 +0800

    Anapolo page complete elements

--- styhrdweb/src/components/AnapoloProj.js
+++ styhrdweb/src/components/AnapoloProj.js
@@ -59,6 +59,23 @@ export const AnapoloProj = ({ onBackToProjects }) => {
                     <p>The system allows users to customize flashcards through CRUD operations: creating new flashcards, reading and viewing existing ones, updating details, and deleting them as needed. This flexibility ensures that users can tailor their study materials to their preferences.</p>
                 </div>
                 <img src='assets/flash.png'></img>
+
+                <div className='stack'>
+                    <div className='tech' >
+                        <p>PHP</p>
+                        <p>MYSQL</p>
+                        <p>APACHE</p>
+                    </div>
+
+                    <p className='featdesc'>
+                        The web application was developed using PHP for server-side scripting, MySQL for database management, and Apache as the web server. PHP handled dynamic content and user interactions, MySQL managed data storage and retrieval, and Apache served as the platform for hosting the application. The hands-on experience gained from creating this thesis application has been valuable in understanding web development intricacies.
+                    </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down icon" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1" />
+                    </svg>
+
+                    <a href='http://anapolothesis.wuaze.com/' target='_blank'>VISIT THE WEBSITE HERE</a>
+                </div>
             </div>
 
 
